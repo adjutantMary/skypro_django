ccf3717f1f43dcbcd60f3df5a710a8aeada3472d
Mariya Polshkova <polshkovamamariya@gmail.com>
2024-07-07 23:02:31 +0300

    created admin service for models

--- task/catalog/admin.py
+++ task/catalog/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Product, Category
 
-# Register your models here.
+
+class MyAdminSite(admin.AdminSite):
+    site_header = "Информация о продуктах"
+
+
+shop_admin = MyAdminSite(name="shop-admin")
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product_name',
+                    'category', 'product_cost')
+    list_filter = ('category',)
+    search_fields = ('product_name', 'product_description')
+    search_help_text = "Поиск работает по названию продукта или его описанию"
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'category_name')
+
+
+shop_admin.register(Product, ProductAdmin)
+shop_admin.register(Category, CategoryAdmin)
