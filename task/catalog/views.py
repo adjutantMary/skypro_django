5da80bcfd4d8d49ea814ecba130314ada7c11e12
Mariya Polshkova <polshkovamamariya@gmail.com>
2024-07-15 22:16:01 +0300

    new views for templates

--- task/catalog/views.py
+++ task/catalog/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Product, Category
 
 
 def home(request):
-    return render(request, "home.html")
+    products = Product.objects.all()
+    categories = Category.objects.all()
+    context = {"products": products,
+               "categories": categories}
+    return render(request, "product_list.html", context)
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {"product": product}
+    return render(request, "product_detail.html", context)
 
 
 def contacts(request):
