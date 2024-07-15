1a14e89bb6e48195225526ff7de36ce367d42ebf
Mariya Polshkova <polshkovamamariya@gmail.com>
2024-07-15 22:16:15 +0300

    edited urls for new views

--- task/catalog/urls.py
+++ task/catalog/urls.py
@@ -1,8 +1,9 @@
 
 from django.urls import path
-from .views import home, contacts
+from .views import home, contacts, product_detail
 
 urlpatterns = [
     path('', home),
-    path('contacts/', contacts)
+    path('contacts/', contacts),
+    path('product/<int:pk>/', product_detail, name='product_detail')
 ]
