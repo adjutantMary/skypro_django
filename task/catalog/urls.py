ed0a245002a9c5d3a23cfa23e1df1f909108096e
Mariya Polshkova <polshkovamamariya@gmail.com>
2024-08-01 01:14:33 +0300

    version urls

--- task/catalog/urls.py
+++ task/catalog/urls.py
@@ -13,4 +13,8 @@ urlpatterns = [
     path("post_list/create/", PostCreateView.as_view(), name='create_view'),
     path("<slug:slug>/update/", PostUpdateView.as_view(), name='post_update'),
     path("<slug:slug>/delete/", PostDeleteView.as_view(), name='post_delete'),
+    path('version/<int:pk>/', VersionDetailView.as_view(), name='version_detail'),
+    path('product/<int:product_id>/version/create/', VersionCreateView.as_view(), name='version_create'),
+    path('version/<int:pk>/edit/', VersionUpdateView.as_view(), name='version_update'),
+    path('version/<int:pk>/delete/', VersionDeleteView.as_view(), name='version_delete'),
 ]
