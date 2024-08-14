b50356786fe0a17e5e63104766c23b2272335bb0
Mariya Polshkova <polshkovamamariya@gmail.com>
2024-08-14 22:25:43 +0300

    кастомная команда для catalog

---
+++ task/management/commands/csu.py
@@ -0,0 +1,13 @@
+from django.core.management import BaseCommand
+from users.models import User
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        User.objects.create(
+            email="admin@gmail.com",
+            is_staff=True,
+            is_superuser=True,
+            is_active=True,
+        )
